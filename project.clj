f51116e5618312106d93b813254695828edefd6e
feldi <peter.feldtmann@gmail.com>
2018-03-25 18:23:59 +0200

    first commit under cursive

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-datomic-tools "0.3.1"
-  :description "Tools for datomic db"
+  :description "Tools for Datomic DB"
   :url "http://github.com/feldi/clj-datomic-tools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.datomic/datomic-free "0.9.5372"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.datomic/datomic-free "0.9.5656"]])
